4db57efb8dd3ad960f3991671cfdf988fade692a
Raman Mishra <2000ramanmishra@gmail.com>
2024-10-01 09:00:39 +0530

    change

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.use((req,res,next)=>{
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/review", reviewRouter);
-app.use("/",userRouter);
+app.use("/listings",userRouter);
 
 app.use((err,req,res,next)=>{
     let{statusCode=500,message="something went wrong!"}= err;
